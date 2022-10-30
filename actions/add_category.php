d76a7ba75a91a6e4caaee84c8678e618f21dac0d
Paa Kwesi <pkaddae56@gmail.com>
2022-10-30 11:04:49 +0000

    Alert and redirect for add category.

--- actions/add_category.php
+++ actions/add_category.php
@@ -1,3 +1,5 @@
+<script src="../js/alertRedirect.js"></script>
+
 <?php
 include "../controllers/product_contrroller.php";
     
@@ -6,11 +8,13 @@ if(ISSET($_POST['categoryinfo'])){
     
     $get_category= $_POST['productcategory'];
 
-    addCategory_ctr($get_category);
+    if( addCategory_ctr($get_category) != NULL){
+        echo '<script>alertRedirect_admin("Record Added","category.php")</script>';
+    }
+    else{
+        print("Record Not Added.");
+    }
 
-    //echo "$get_brand";
-    
-    header("Location: ../admin/category.php");
     exit();
 }
 ?>
