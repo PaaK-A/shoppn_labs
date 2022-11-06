f3a87c927f33871c52c5db06aa2b66ef60dd48b9
Paa Kwesi <pkaddae56@gmail.com>
2022-11-06 20:05:59 +0000

    running edit product using controller

---
+++ actions/edit_product.php
@@ -0,0 +1,50 @@
+<script src="../js/alertRedirect.js"></script>
+
+<?php
+include("../settings/core.php");
+include "../controllers/product_contrroller.php";
+
+$getproductID= $_SESSION['id'];
+
+
+//collecting form data
+if(ISSET($_POST['editproductinfo'])){
+    //get user input
+    $get_editedpdtcategory= $_POST['editedpcat'];
+    $get_editedpdtbrand= $_POST['editedpdtbrand'];
+    $get_editedpdttitle= $_POST['editedpdtTitle'];
+    $get_editedpdtprice= $_POST['editedpdtPrice'];
+    $get_editedpdtdescription= $_POST['editedpdtDescription'];
+    $get_editedpdtimage= $_POST['editedpdtImg'];
+    $get_editedpdtkeywords= $_POST['editedpdtKeywords'];
+
+
+    //update in db
+    if (updateProduct_ctr($get_editedpdtcategory,$get_editedpdtbrand,$get_editedpdttitle,$get_editedpdtprice,$get_editedpdtdescription,$get_editedpdtimage,$get_editedpdtkeywords,$getproductID) != NULL){
+        //print("Record Updated.");
+        echo 
+        '<script type="text/javascript">
+        alertRedirect_view("Product Updated.","addproduct_form.php");
+        </script>';
+    }
+    else{
+        print("Record Not Updated.");
+    };
+    
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+     <!--back to index button-->
+     <button type="button" onclick="location.href='../view/addproduct_form.php'">Return to Product</button>
+</body>
+</html>
