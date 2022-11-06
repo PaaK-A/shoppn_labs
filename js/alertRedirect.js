d32a2e74715fbe5ba10071c7f2bd58c83d532c4e
Paa Kwesi <pkaddae56@gmail.com>
2022-11-06 20:11:42 +0000

    js redirect to index, view folder

--- js/alertRedirect.js
+++ js/alertRedirect.js
@@ -4,8 +4,20 @@ function alertRedirect(){
     location.replace("../admin/category.php");
 }
 
+function alertRedirect_index(message){
+        
+    alert(message);
+    location.replace("../index.php");
+}
+
 function alertRedirect_admin(message,redirect_destination){
         
     alert(message);
     location.replace("../admin/"+redirect_destination);
+}
+
+function alertRedirect_view(message,redirect_destination){
+        
+    alert(message);
+    location.replace("../view/"+redirect_destination);
 }
