cb5350b69baa09088cfd7ec74e8db2d2d63e7d86
Paa Kwesi <pkaddae56@gmail.com>
2022-11-08 21:01:36 +0000

    Move to images folder and path in db

--- actions/add_product.php
+++ actions/add_product.php
@@ -12,16 +12,28 @@ if(ISSET($_POST['productinfo'])){
     $get_productdescription=$_POST['productDescription'];
     $get_productimage=$_POST['productImg'];
     $get_productkeywords=$_POST['productKeywords'];
+    $imgtmp_name= $_FILES['productImg']['tmp_name'];
+    $imgfile_name= $_FILES['productImg']['name'];
+    $folder= "../images/".$imgfile_name;
+    
+    //echo $file;
 
-
-     
-    if( addProduct_ctr($get_productcat,$get_productbrand,$get_producttitle, $get_productprice,$get_productdescription,$get_productimage,$get_productkeywords) != NULL){
-        echo '<script>alertRedirect_index("Product Added")</script>';
-        //echo 'Record added.';
+    if(move_uploaded_file($imgtmp_name, $folder)){
+        //echo "Image moved.";
+        //echo $folder;
+        if( addProduct_ctr($get_productcat,$get_productbrand,$get_producttitle, $get_productprice,$get_productdescription,$folder,$get_productkeywords) != NULL){
+            echo '<script>alertRedirect_index("Product Added")</script>';
+            echo 'Record added.';
+        }
+        else{
+            print("Record Not Added.");
+        }
     }
     else{
-        print("Record Not Added.");
+        echo "Image not moved.";
     }
+    
+    
     
 }
 
