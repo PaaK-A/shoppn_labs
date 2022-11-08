4e66b170c61ec63cf643d8b58e0e2ce012f5a81c
Paa Kwesi <pkaddae56@gmail.com>
2022-11-08 21:07:34 +0000

    basic navigation bar

---
+++ view/nav_footer.php
@@ -0,0 +1,76 @@
+<?php
+    // Initialize the session
+    //session_start();
+    include_once("../settings/core.php");
+    include_once("../controllers/product_contrroller.php");
+
+    // Check if the user is logged in, if not then redirect him to login page
+    //if logged in check if admin, if not redirect to login page 
+
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+
+        if($_SESSION["role"] == 1){ 
+            //echo "Hello admin";
+            $role= "ADMIN";
+        }
+
+        else{ 
+            //echo "You are not ADMIN.";
+            $role= "User";
+            //echo '<a href="l../login/login.php"">Login</a>';
+        }
+    }
+
+    else{
+        header("location: ../login/login.php");
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../css/styles.css" rel="stylesheet" />
+</head>
+<body>
+     <!-- Navigation-->
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="../index.php">LABS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="all_product.php">All Products</a></li>
+                        <!-- <li class="nav-item"><a class="nav-link" href="#!">About</a></li> -->
+                        <!-- <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="/Applications/XAMPP/xamppfiles/htdocs/shoppn_labs/view/all_product.php">All Products</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            </ul>
+                        </li> -->
+                    </ul>
+                    <!-- <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form> -->
+                </div>
+            </div>
+        </nav>
+</body>
+</html>
