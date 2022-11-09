91a943395f5ee676930c208003e8b7009b919eb2
Paa Kwesi <pkaddae56@gmail.com>
2022-11-09 15:14:26 +0000

    Revert "error commit"
    This reverts commit 4187f30dfbe2b857a7f5efab35472825e520e94b.

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,14 +1,14 @@
 <?php
 //Database credentials
 
-define("SERVER","localhost");
-define("USERNAME","root");
-define("PASSWD","");
-define("DATABASE","id19735912_shoppn");
+// define("SERVER","localhost");
+// define("USERNAME","root");
+// define("PASSWD","");
+// define("DATABASE","id19735912_shoppn");
 
-// define("SERVER","eu-cdbr-west-03.cleardb.net");
-// define("USERNAME","b2b98df45e17a9");
-// define("PASSWD","8c3e4360");
-// define("DATABASE","heroku_0a872341616352c");
+define("SERVER","eu-cdbr-west-03.cleardb.net");
+define("USERNAME","b2b98df45e17a9");
+define("PASSWD","8c3e4360");
+define("DATABASE","heroku_0a872341616352c");
 
 ?>
